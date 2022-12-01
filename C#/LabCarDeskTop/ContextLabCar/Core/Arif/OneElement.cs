a78b695de178a2e53eb0aa772c4321a462857a2e
Alexandr Lanin <alexandr.lanin@nami.ru>
2022-12-01 11:41:55 +0300

    Refacting

--- C#/LabCarDeskTop/ContextLabCar/Core/Arif/OneElement.cs
+++ C#/LabCarDeskTop/ContextLabCar/Core/Arif/OneElement.cs
@@ -1,14 +1,20 @@
-﻿
+﻿using DryIoc.ImTools;
+
 public interface IOneElement
 {
 }
 public class OneElement : IOneElement
 {
-  public string Name { get; set; }
-  public string? NameValue { get; set; }
-  public string? CommandAri { get; set; }
-  private readonly CalcElement? _calcElement;
+  private string Name { get; set; }
+  private string? NameValue { get; set; }
+  private string? CommandAri { get; set; }
+  
   public CVariable CVariable { get; set; }
+
+  private string BasaCommanda { get; set; }
+
+  private readonly string _nameTree = "__#";
+  private int _indexCom;
 
   public OneElement(string commandAri, string name="")
   {
@@ -30,20 +36,111 @@ public class OneElement : IOneElement
       CommandAri = strBasa;
       Name = name;
     }
-    _calcElement = new(Name);
+    BasaCommanda = CommandAri;
+    _indexCom = 0;
+    _nameTree = _nameTree + name;
   }
 
   public CVariable FuncCalc()
   {
-    if (_calcElement == null) return new CVariable("");
-
-    _calcElement.CaclScobki(CommandAri);
-    _calcElement.BasaCommanda = _calcElement.ReplaseSimvolDan(_calcElement.BasaCommanda);
-    _calcElement.BasaCommanda = _calcElement.ReplaseMultiDiv(_calcElement.BasaCommanda);
-    _calcElement.BasaCommanda = _calcElement.FindNonNumbers(_calcElement.BasaCommanda, NameValue);
+    CaclScobki(CommandAri);
+    BasaCommanda = ReplaseSimvolDan(BasaCommanda);
+    BasaCommanda = ReplaseMultiDiv(BasaCommanda);
+    BasaCommanda = FindNonNumbers(BasaCommanda, NameValue);
 
     CVariable = StArithmetic.DVarCommand[NameValue];
     return CVariable;
   }
 
+  public void CaclScobki(string scobki)
+  {
+    BasaCommanda = scobki;
+    while (StArithmetic.IsScobki(scobki).Item1)
+    {
+      var st0 = scobki;
+      var xScop = StArithmetic.ScobkiX(scobki);
+
+      if (xScop.Count <= 0)
+        continue;
+
+      var x0 = xScop.ElementAt(0);
+
+      var ssx = scobki.Substring(x0.Item1, x0.Item2 - x0.Item1 + 1);
+      var nameTreeX = _nameTree + _indexCom;
+      st0 = st0.Replace(ssx, nameTreeX);
+      ssx = ssx.Replace("(", "").Replace(")", "");
+
+      var cv = new CVariable(nameTreeX + "=" + ssx);
+      if (!cv.IsValue)
+        StArithmetic.DVarCommand.AddOrUpdate(nameTreeX, cv, (_, _) => cv);
+
+      xScop.RemoveAt(0);
+      scobki = st0;
+      _indexCom++;
+    }
+    BasaCommanda = scobki;
+  }
+
+  public string ReplaseSimvolDan(string str)
+  {
+    var keyVal = StArithmetic.DVarCommand.Where(z => z.Value.IsValue)
+      .Select(x => new { x.Key, x.Value.Value })
+      .ToDictionary(item => item.Key, item => ((string)Convert.ToString(item.Value)).Replace(',', '.'));
+
+    foreach (var it in keyVal
+               .Where(it => str.Contains(it.Key) && it.Key.Contains(_nameTree)))
+    {
+      str = str.Replace(it.Key, it.Value);
+      _ = StArithmetic.DVarCommand.TryRemove(it.Key, out var xx);
+    }
+    return str;
+  }
+
+  public string ReplaseMultiDiv(string str)
+  {
+    var countMultiDiv = StArithmetic.IsUmnDiv(str);
+    if (!countMultiDiv.Item1) return str;
+
+    var dls = StArithmetic.SplitPlusMin(str).Where(x => x.Contains("*") || x.Contains("/")).ToList();
+
+    Dictionary<string, dynamic?> dyn0 = new();
+    foreach (var d in dls)
+    {
+      var xxx = StArithmetic.MultiDiv(d);
+      if (xxx != null)
+        str = str.Replace(d, (string)Convert.ToString(xxx));
+    }
+    return str;
+  }
+
+  public string FindNonNumbers(string str, string nameX = "")
+  {
+    var countMultiDiv = StArithmetic.IsUmnDiv(str);
+    var countPlusMin = StArithmetic.IsPlusMin(str);
+    if (!countMultiDiv.Item1 && countPlusMin.Item1) return str;
+
+    var countDigital = StArithmetic.SplitDigital(str);
+    foreach (var it in countDigital.Where(x => !StArithmetic.IsDigitString(x)))
+    {
+      if (StArithmetic.DVarCommand.TryGetValue(it, out var dan))
+      {
+        if (!dan.IsValue)
+        {
+          var s0 = FindNonNumbers(dan.StValue, it);
+          if (StArithmetic.DVarCommand[it].IsValue)
+            str = str.Replace(it, StArithmetic.DVarCommand[it].SValue);
+        }
+        else
+          str = str.Replace(it, Convert.ToString(dan.Value));
+
+      }
+    }
+    str = (nameX == "" ? "root" : nameX) + "=" + ReplaseMultiDiv(str);
+
+    var cv = new CVariable(str);
+    return str;
+
+  }
+
 }
+
